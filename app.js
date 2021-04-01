c1a5f50aa1bd13576113ecf3924a9c70e1547fc4
Narendra-kumar-Gunturu <77824814+Narendra-kumar-Gunturu@users.noreply.github.com>
2021-03-31 22:29:59 -0500

    updated files

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var chipsRouter = require('./routes/chips');
+var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
+
+
+
 
 
 var app = express();
@@ -24,6 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chips', chipsRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
