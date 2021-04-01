5a6fb43d4eda5234da1703e6cc36e4ec72516bbc
Narendra-kumar-Gunturu <77824814+Narendra-kumar-Gunturu@users.noreply.github.com>
2021-03-31 22:04:37 -0500

    updated files

--- routes/chips.js
+++ routes/chips.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('chips', { title: 'Search Results' });
+  res.render('chips', { title: 'Search Results chips' });
 });
 
 module.exports = router;
